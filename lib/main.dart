4269f51eb21a591115ca7d48c576a08d8d37b623
rawarashid19 <rawarashid19@gmail.com>
2023-05-16 23:51:37 +0300

    The text from Main.dart has been chnaged

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  runApp(MaterialApp(home: Text('Rawa Rashid'),));
+  runApp(MaterialApp(home: Text('Rawa Rashid / This is The first App'),));
 
 }
 
